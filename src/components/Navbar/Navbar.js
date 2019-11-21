40b6b3383771439d1e1e514eed4874c248a36abb
Sourav Debnath <sourav2012d@gmail.com>
2019-11-21 22:45:30 +0530

    Adjusts side and top nav height

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,13 +10,12 @@ import {
 
 const NavBar = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home"></Navbar.Brand>
+    <Navbar bg="light" expand="lg" style={{ fontSize: 20, height: 84 }}>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mx-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link" className="text-primary">
+          <Nav.Link href="#link" className="text-primary" id="paddingClass">
             Sign In
           </Nav.Link>
           <Nav.Link href="Help">Help</Nav.Link>
