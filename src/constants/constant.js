40a183bbcc663c9025a7bc12e69eca9d2a576fb0
Shiva <pythonshiva@users.noreply.github.com>
2019-11-26 17:52:18 +0530

    updated link for api

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,5 +1,5 @@
 export const AWS_URL =
-  "https://gzfeybe2ai.execute-api.ap-south-1.amazonaws.com/default/MyServerless";
+  "https://3z413fai9a.execute-api.ap-south-1.amazonaws.com/default/MyServerless";
 export const AWS_METHOD = "GET";
 
 // No Security Token Has Been Provided, open endpoint
